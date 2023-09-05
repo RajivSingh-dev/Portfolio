a86105a27e9df89d7611beff18f2a60e99117884
Rajiv Singh <rajiv.singh@aas.services>
2023-09-06 01:46:08 +0530

    Adding services and configurations

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,14 +1,22 @@
+using Portfolio.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // getting runtime environment
 var environment = builder.Environment.EnvironmentName;
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{environment}.json", true, true)
+    .AddEnvironmentVariables()
+    .Build();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
 
+string portfolioConnection = config.GetConnectionString("PortfolioConnection");
 
-
-
+builder.Services.AddDbContext<PortfolioContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
